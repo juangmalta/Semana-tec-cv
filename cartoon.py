8aae4dbc469db9fc1b38824cd9bd0eb931228018
m4nu3l01 <A01639643@itesm.mx>
2021-09-15 19:05:00 -0500

    Update cartoon.py

--- cartoon.py
+++ cartoon.py
@@ -1,14 +1,15 @@
 import cv2
 import numpy as np
 
+#image
 img=cv2.imread("manuel.jpeg")
 
-
+#Edges
 gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray=cv2.medianBlur(gray,5)
 edges=cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,9,9)
 
-
+#Cartoon
 color=cv2.bilateralFilter(img,9,250,250)
 cartoon=cv2.bitwise_and(color,color,mask=edges)
 
